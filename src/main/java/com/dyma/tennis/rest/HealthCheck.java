550d8c10893d1e7495e2f366ca4e1209fe473444
Aurélien <aurelien@ordinateur-portable-de-aurelien.home>
2023-08-08 23:05:34 +0200

    :sparkles: init healthcheck rest controller

---
+++ src/main/java/com/dyma/tennis/rest/HealthCheck.java
@@ -0,0 +1,4 @@
+package com.dyma.tennis.rest;
+
+public record HealthCheck(ApplicationStatus status, String message) {
+}
---
+++ src/main/java/com/dyma/tennis/rest/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.dyma.tennis.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public HealthCheck healthcheck() {
+        return new HealthCheck(ApplicationStatus.OK, "Welcome to Dyma Tennis!");
+    }
+}
